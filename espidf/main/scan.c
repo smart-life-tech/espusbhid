49fd0a22c230fb13c2de22ba20c2105daae09836
smart-life-tech <christlightworld@gmail.com>
2024-12-23 14:17:37 +0100

    modified:   espidf/main/scan.c

--- espidf/main/scan.c
+++ espidf/main/scan.c
@@ -1,15 +1,4 @@
-/* Scan Example
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
-/*
-    This example shows how to scan for available set of APs.
-*/
 #include <string.h>
 #include <stdlib.h>
 #include "freertos/FreeRTOS.h"
